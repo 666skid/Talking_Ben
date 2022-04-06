7183f527b127a127f8020a65a95ed3bb2d44c192
666skid <baohuu2007@gmail.com>
2022-04-06 02:14:06 +0000

    commit

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ Clientdiscord = discord.Client()
 async def on_message(message):
     if client.user.mentioned_in(message):
         variable = [
-            'https://cdn.discordapp.com/attachments/946549502590976040/947015921107628054/video-output-9D64EA54-2F73-42A9-9A23-738F2ACA1FB5.mov',
-            'https://media.discordapp.net/attachments/945616095820279818/949501258732040202/video-output-1048F815-A992-4B05-B1A2-5CAB0851CDC1.mov',
-            'https://cdn.discordapp.com/attachments/946549502590976040/947016570666909776/video-output-15DFA988-4E24-494F-8938-8E17816923EE.mov',]
-        await message.channel.send(random.choice(variable))
+            'ben/yes.mov',
+            'ben/no.mov',
+            'ben/hohoho.mov']
+        await message.reply(file=discord.File(random.choice(variable)))
 
 
 
